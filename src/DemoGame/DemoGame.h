0dbf7afa0a85dd8df234a3fbda405be4a2a574d5
Andrea Lam <lam20102@uvg.edu.gt>
2024-11-11 18:30:05 -0600

    Sprite movimiento
    desaparece al momento de mover hacia arriba e izquierda

--- src/DemoGame/DemoGame.h
+++ src/DemoGame/DemoGame.h
@@ -5,23 +5,41 @@
 #include "Engine/Systems.h"
 #include "Engine/Graphics/TextureManager.h"
 #include "Engine/Graphics/Texture.h"
+#include <SDL_events.h>
+#include <SDL_timer.h>
 #include <entt/entt.hpp>
 
+struct TextureComponent {
+  std::string filename;
+};
+
+struct BackgroundComponent {
+  std::string filename;
+};
+
+struct PlayerComponent {
+
+};
+
 struct SpriteComponent {
+  std::string filename;
   int width;
   int height;
-  SDL_Color color;
+  int scale = 1;
+  int animationFrames = 0;
+  int animationDuration = 0;
+  Uint32 lastUpdate = 0;
+  int xIndex = 0;
+  int yIndex = 0; 
 };
 
 class SquareSpawnSetupSystem : public SetupSystem {
   void run() {
     Entity* square = scene->createEntity("SQUARE", 10, 10); 
-    square->addComponent<VelocityComponent>(500, 500);
-    square->addComponent<SpriteComponent>(100, 100, SDL_Color{255, 0, 0});
-
-    Entity* square2 = scene->createEntity("SQUARE2", 924, 10); 
-    square2->addComponent<VelocityComponent>(-400, -400);
-    square2->addComponent<SpriteComponent>(100, 100, SDL_Color{0, 0, 255});
+    square->addComponent<PlayerComponent>();
+    square->addComponent<VelocityComponent>(300);
+    square->addComponent<TextureComponent>("src/images/ballena.png");
+    square->addComponent<SpriteComponent>("src/images/ballena.png", 64, 64);
   }
 };
 
@@ -39,6 +57,68 @@ class MovementSystem : public UpdateSystem {
   }
 };
 
+class SpriteMovementSystem : public UpdateSystem {
+  void run(float dT) {
+    auto view = scene->r.view<SpriteComponent, VelocityComponent>();
+
+    for (auto e : view) {
+      auto& spr = view.get<SpriteComponent>(e);
+      auto vel = view.get<VelocityComponent>(e);
+
+      if (vel.x == 0) {
+        if (spr.yIndex == 1) {
+          spr.yIndex = 0;
+        } else if (spr.yIndex == 3) {
+          spr.yIndex = 2;
+        }
+      }
+      if (vel.x < 0 || vel.y < 0) {
+        spr.yIndex = 3;
+      }
+      if (vel.x > 0 || vel.y > 0) {
+        spr.yIndex = 1;
+      }
+   }
+  }
+};
+
+class MovementInputSystem : public EventSystem {
+  void run(SDL_Event event) {
+   auto view = scene->r.view<PlayerComponent, VelocityComponent>();
+    for (auto e : view) {
+      auto& vel = view.get<VelocityComponent>(e);
+      
+      if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_LEFT) {
+          vel.x = -vel.speed;
+        }
+        if (event.key.keysym.sym == SDLK_RIGHT) {
+          vel.x = vel.speed;
+        }
+        if (event.key.keysym.sym == SDLK_UP) {
+          vel.y = -vel.speed;
+        }
+        if (event.key.keysym.sym == SDLK_DOWN) {
+          vel.y = vel.speed;
+        }
+      } else if (event.type == SDL_KEYUP) {
+        if (event.key.keysym.sym == SDLK_LEFT) {
+          vel.x = 0;
+        }
+        if (event.key.keysym.sym == SDLK_RIGHT) {
+          vel.x = 0;
+        }
+        if (event.key.keysym.sym == SDLK_UP) {
+          vel.y = 0;
+        }
+        if (event.key.keysym.sym == SDLK_DOWN) {
+          vel.y = 0;
+        }
+      }
+    }
+  }
+};
+
 class WallHitSystem : public UpdateSystem {
   void run(float dT) {
     auto view = scene->r.view<PositionComponent, VelocityComponent, SpriteComponent>();
@@ -51,11 +131,11 @@ class WallHitSystem : public UpdateSystem {
       int newPosX = pos.x + vel.x * dT;
       int newPosY = pos.y + vel.y * dT;
 
-      if (newPosX < 0 || newPosX + spr.width > 1024) {
+      if (newPosX < 0 || newPosX + spr.width > 400) {
         vel.x *= -1.1;
       }
 
-      if (newPosY < 0 || newPosY + spr.height > 768) {
+      if (newPosY < 0 || newPosY + spr.height > 600) {
         vel.y *= -1.1;
       }
 
@@ -63,35 +143,63 @@ class WallHitSystem : public UpdateSystem {
   }
 };
 
-class SquareRenderSystem : public RenderSystem {
+class SpriteAnimationSystem : public UpdateSystem {
+  void run(float dT) override {
+    auto view = scene->r.view<SpriteComponent>();
+    Uint32 now = SDL_GetTicks();
+
+    for (auto e : view) {
+      auto& spr = view.get<SpriteComponent>(e);
+
+      if (spr.animationFrames > 0) {
+        if (spr.lastUpdate == 0) {
+          spr.lastUpdate = now;
+          continue;
+        }
+        float timeSinceLastUpdate = now - spr.lastUpdate;
+
+        int lastFrame = spr.animationFrames - 1;
+
+        int framesToUpdate = timeSinceLastUpdate / spr.animationDuration * spr.animationFrames; 
+
+        if (framesToUpdate > 0) {
+          spr.xIndex += framesToUpdate;
+          spr.xIndex %= spr.animationFrames;
+          spr.lastUpdate = now;
+        }
+
+      }
+    }
+  }
+};
+
+class SpriteRenderSystem : public RenderSystem {
   void run(SDL_Renderer* renderer) {
     auto view = scene->r.view<PositionComponent, SpriteComponent>();
     for (auto e : view) {
       auto pos = view.get<PositionComponent>(e);
       auto spr = view.get<SpriteComponent>(e);
 
-      SDL_SetRenderDrawColor(renderer, spr.color.r, spr.color.g, spr.color.b, spr.color.a);
-      SDL_Rect r = { pos.x, pos.y, spr.width, spr.height };
-      SDL_RenderFillRect(renderer, &r);
+      Texture* texture = TextureManager::GetTexture(spr.filename);
+      SDL_Rect clip = {
+        spr.xIndex * spr.width,
+        spr.yIndex * spr.height,
+        spr.width,
+        spr.height,
+      };
+      texture->render(scene->renderer, pos.x, pos.y, spr.width * spr.scale, spr.height * spr.scale, &clip);
     }
   }
 }; 
-
-
-struct TextureComponent {
-  std::string filename;
-};
-
-struct BackgroundComponent {
-  std::string filename;
-};
 
 class BackgroundSetupSystem : public SetupSystem {
 public:
   void run() override {
     Entity* background = scene->createEntity("BACKGROUND");
     const std::string& bgfile = "src/images/Ocean.png";
+    background->addComponent<PositionComponent>(0, 0);
     background->addComponent<TextureComponent>(bgfile);
+    background->addComponent<SpriteComponent>(bgfile, 400, 600, 1, 3, 500);
     background->addComponent<BackgroundComponent>(bgfile);
   }
 };
@@ -106,18 +214,6 @@ class TextureSetupSystem : public SetupSystem {
   }
 }; 
 
-class BackgroundRenderSystem : public RenderSystem {
-  void run(SDL_Renderer* renderer) {
-    auto view = scene->r.view<BackgroundComponent>();
-    for (auto e : view) {
-      auto tex = view.get<BackgroundComponent>(e);
-      auto texture = TextureManager::GetTexture(tex.filename);
-      texture->render(renderer, 0, 0);
-    }
-  }
-}; 
-
-
 class DemoGame : public Game {
 public:
   Scene* sampleScene;
@@ -128,17 +224,17 @@ public:
   : Game("SAMPLE", 400, 600)
   { }
 
-  void setup() {
-      
+  void setup() { 
     sampleScene = new Scene("SAMPLE SCENE", r, renderer);
     addSetupSystem<SquareSpawnSetupSystem>(sampleScene);
     addSetupSystem<BackgroundSetupSystem>(sampleScene);
     addSetupSystem<TextureSetupSystem>(sampleScene);
-
+    addEventSystem<MovementInputSystem>(sampleScene);
+    addUpdateSystem<SpriteMovementSystem>(sampleScene);
     addUpdateSystem<MovementSystem>(sampleScene);
+    addUpdateSystem<SpriteAnimationSystem>(sampleScene);
     addUpdateSystem<WallHitSystem>(sampleScene);
-    addRenderSystem<BackgroundRenderSystem>(sampleScene);
-    addRenderSystem<SquareRenderSystem>(sampleScene);
+    addRenderSystem<SpriteRenderSystem>(sampleScene);
 
     setScene(sampleScene);
   }
